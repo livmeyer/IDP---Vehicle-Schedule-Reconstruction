5f065e028b738b0d2cb5264315bda122832da505
Olivia Meyer <olivia-meyer-02@outlook.de>
2026-09-17 14:55:59 +0200

    Interlining Trips added and  csv files formated

--- Scheduler.py
+++ Scheduler.py
@@ -24,9 +24,9 @@ def seconds_to_time(total_seconds: int) -> str:
 
 def schedule_tasks(tasks: pd.DataFrame, start_vehicle_id_counter: int, min_terminal_sec: int, max_terminal_sec: int,
                    deadhead_lookup: DeadheadDistanceLookup, interlining_mapping: dict[str, dict[str, str]] = {}
-                   ) -> tuple[defaultdict[int, list[str]], list[dict[str, str | bool | int]], int, int]:
+                   ) -> tuple[dict[str,int], list[dict[str, str | bool | int]], int, int]:
     available_vehicles: defaultdict[int, list[str]] = defaultdict(list)
-    vehicles_and_trips: defaultdict[int, list[str]] = defaultdict(list)
+    vehicles_and_trips: dict[str, int] = {}
     tasks["time_sec"] = tasks["time"].apply(time_to_seconds)
     assigned_vehicles = {}
 
@@ -35,6 +35,8 @@ def schedule_tasks(tasks: pd.DataFrame, start_vehicle_id_counter: int, min_termi
 
     vehicle_first_trip = {}
     vehicle_last_trip = {}
+
+    deadhead_interlining_rows: list[dict[str, str | bool | int]] = []
 
     for trip in tasks.itertuples():
         t_sec = trip.time_sec
@@ -59,17 +61,36 @@ def schedule_tasks(tasks: pd.DataFrame, start_vehicle_id_counter: int, min_termi
                 vehicle_id_counter += 1
 
             v_assigned = assigned_vehicles[trip.trip_id]
-            vehicles_and_trips[v_assigned].append(trip.trip_id)
+            vehicles_and_trips[trip.trip_id] = v_assigned
 
             if v_assigned not in vehicle_first_trip:
                 vehicle_first_trip[v_assigned] = trip
 
         else:
             if (trip.route_id in interlining_mapping) and (trip.parent_station in interlining_mapping[trip.route_id]):
-                dh_time = deadhead_lookup.get_duration(trip.stop_id, interlining_mapping[trip.route_id][trip.parent_station])
+                destination_id: str = interlining_mapping[trip.route_id][trip.parent_station]
+                dh_time: int = deadhead_lookup.get_duration(trip.stop_id, destination_id)
                 ready_sec = t_sec + dh_time
-                max_wait_sec = t_sec +  dh_time + max_terminal_sec
+                max_wait_sec = t_sec + dh_time + max_terminal_sec
                 non_service_sec += dh_time
+
+                interlining_id: str = f"interlining_{trip.trip_id}"
+
+                deadhead_interlining_rows.extend([
+                    {
+                        "stop_id": trip.stop_id,
+                        "route_id": interlining_id,
+                        "trip_id": trip.trip_id,
+                        "time": seconds_to_time(t_sec),
+                        "start": True
+                    },
+                    {
+                        "stop_id": destination_id,
+                        "route_id": interlining_id,
+                        "trip_id": trip.trip_id,
+                        "time": seconds_to_time(t_sec + dh_time),
+                        "start": False,
+                    }, ])
             else:
                 ready_sec = t_sec + min_terminal_sec
                 max_wait_sec = t_sec + max_terminal_sec
@@ -78,8 +99,6 @@ def schedule_tasks(tasks: pd.DataFrame, start_vehicle_id_counter: int, min_termi
             heapq.heappush(available_vehicles[station], (ready_sec, max_wait_sec, v_id))
 
             vehicle_last_trip[v_id] = trip
-
-    deadhead_rows: list[dict[str, str | bool | int]] = []
 
     for vehicle in range(start_vehicle_id_counter, vehicle_id_counter):
         first_trip = vehicle_first_trip[vehicle]
@@ -108,48 +127,41 @@ def schedule_tasks(tasks: pd.DataFrame, start_vehicle_id_counter: int, min_termi
         station_arrive_sec = first_time_sec - min_terminal_sec
         depot_return_sec = last_time_sec + int(deadhead_end)
 
-        deadhead_rows.extend([
+        deadhead_interlining_rows.extend([
             {
                 "stop_id": depot,
                 "route_id": first_route,
                 "trip_id": depart_trip_name,
                 "time": seconds_to_time(depot_depart_sec),
-                "time_sec": depot_depart_sec,
                 "start": True,
-                "parent_station": "",
             },
             {
                 "stop_id": first_stop,
                 "route_id": first_route,
                 "trip_id": depart_trip_name,
                 "time": seconds_to_time(station_arrive_sec),
-                "time_sec": station_arrive_sec,
                 "start": False,
-                "parent_station": "",
             },
             {
                 "stop_id": last_stop,
                 "route_id": last_route,
                 "trip_id": return_trip_name,
                 "time": seconds_to_time(last_time_sec),
-                "time_sec": last_time_sec,
                 "start": True,
-                "parent_station": "",
             },
             {
                 "stop_id": depot,
                 "route_id": first_route,
                 "trip_id": return_trip_name,
                 "time": seconds_to_time(depot_return_sec),
-                "time_sec": depot_return_sec,
                 "start": False,
-                "parent_station": "",
             },
         ])
 
-        vehicles_and_trips[vehicle].extend([depart_trip_name, return_trip_name])
+        vehicles_and_trips[depart_trip_name] = vehicle
+        vehicles_and_trips[return_trip_name] = vehicle
 
-    return [vehicles_and_trips, deadhead_rows, vehicle_id_counter, non_service_sec]
+    return [vehicles_and_trips, deadhead_interlining_rows, vehicle_id_counter, non_service_sec]
 
 
 def build_schedule(
@@ -169,7 +181,7 @@ def build_schedule(
     trips["route_short_name"] = trips["route_id"].map(route_map)
 
     vehicle_id_counter = 0
-    vehicles_and_trips: list[defaultdict[int, list[str]]] = []
+    vehicles_and_trips: dict[str, int] = {}
     total_nonservice_sec = 0
     deadhead_rows: list[dict[str, str | bool | int]] = []
     route_vehicle_num: dict[str, tuple[int, int]] = {}
@@ -180,23 +192,32 @@ def build_schedule(
         )
         if len(tasks_on_route) >= 8:
             vnt, dhr, vi, non_service_sec = schedule_tasks(tasks_on_route, vehicle_id_counter, min_terminal_sec, max_terminal_sec, deadhead_lookup)
-            vehicles_and_trips.append(vnt)
+            vehicles_and_trips.update(vnt)
             route_vehicle_num[route_name] = [vi - vehicle_id_counter, non_service_sec]
             vehicle_id_counter = vi
-            deadhead_rows.append(dhr)
+            deadhead_rows.extend(dhr)
             total_nonservice_sec += non_service_sec
 
-    output_trips = trips.drop(columns=["time_sec", "route_short_name"], errors="ignore")
+    trips = pd.concat([trips.drop(columns=["time_sec", "route_short_name", "parent_station"], errors="ignore"), pd.DataFrame(deadhead_rows)],
+                      ignore_index=True)
 
-    output_trips.to_csv("sup_trips.csv", index=False)
+    trips = pd.merge(
+        trips[trips["start"]][["stop_id", "route_id", "trip_id", "time"]]
+        .rename(columns={"time": "departure_time", "stop_id": "start_terminal_id"}),
 
-    with open("sup_vehicleAssignments.csv", "w", newline="", encoding="utf-8") as f:
-        writer = csv.writer(f)
-        writer.writerow(["vehicle_id", "trip_id"])
-        for d in vehicles_and_trips:
-            for vehicle, tasks in d.items():
-                for task in tasks:
-                    writer.writerow([vehicle, task])
+        trips[~trips["start"]][["stop_id", "trip_id", "time"]]
+        .rename(columns={"time": "arrival_time", "stop_id": "arrival_terminal_id"}),
+
+        on="trip_id",
+        how="inner"
+    )
+
+    vehicle_assignment_df = pd.DataFrame(vehicles_and_trips.items(), columns=["trip_id", "vehicle_id"])
+    trips = trips.merge(vehicle_assignment_df, on="trip_id", how="inner")
+
+    trips.to_csv("sup_trips.csv", index=False)
+    vehicle_assignment_df.to_csv("sup_vehicleAssignments.csv", index=False)
+
 
     print(f"Total nonoperational time (seconds): {total_nonservice_sec} ({seconds_to_time(total_nonservice_sec)})")
     return route_vehicle_num
